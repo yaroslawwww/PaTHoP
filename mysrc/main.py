f17ec98046427781f161f314f697086558815bf4
Yaroslav Vasiliev <ikvasilev@edu.hse.ru>
2025-02-06 11:56:42 +0300

    я идиот

--- mysrc/main.py
+++ mysrc/main.py
@@ -4,9 +4,11 @@ from Research import *
 def main():
 
     baranov_result = threaded_research(r_values=[28],gap_number=20)
-    my_result = threaded_research(r_values=[28,25,27], gap_number=20)
+    my_result = threaded_research(r_values=[28,25,27,29,30,31], gap_number=20)
     with open("output.txt", "w") as file:
-        file.write(str(baranov_result)+'\n')
-        file.write(str(my_result))
+        file.write("Результат для одного ряда")
+        file.write(str(round(baranov_result,5))+'\n')
+        file.write("Результат для 6 рядов")
+        file.write(str(round(my_result,5)))
 if __name__ == '__main__':
     main()
