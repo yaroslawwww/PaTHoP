7791abb36a69d1193f9076a55a31207a1b954c25
Yaroslav Vasiliev <ikvasilev@edu.hse.ru>
2025-02-06 10:27:01 +0300

    проверка на фулл nan

--- mysrc/Research.py
+++ mysrc/Research.py
@@ -50,6 +50,8 @@ def research(min_window_index, max_window_index, r_values=None, test_size_consta
         if len(real_values) != len(pred_values):
             return rmses
         mask = ~np.isnan(real_values) & ~np.isnan(pred_values)
+        if np.all(np.isnan(real_values)):
+            continue
         rmses.append(mean_squared_error(real_values[mask], pred_values[mask]))
         # print(mean_squared_error(real_values[mask], pred_values[mask]))
     return rmses
