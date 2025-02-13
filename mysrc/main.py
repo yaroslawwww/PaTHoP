d79aab66a4c752e5fff66ebfb421152680372d39
Yaroslav Vasiliev <ikvasilev@edu.hse.ru>
2025-02-13 21:15:49 +0300

    Исследование длины дополнительного ряда

--- mysrc/main.py
+++ mysrc/main.py
@@ -5,107 +5,58 @@ from matplotlib import pyplot as plt
 import concurrent.futures
 import seaborn as sns
 
-def process_epsilon(epsilon, shares, size, divisor):
+def process_size(epsilon,sizes, divisor):
     # Каждый процесс выполняет вызов threaded_research с заданным epsilon
     result = threaded_research(r_values=[28, 28 + epsilon],
-                               ts_size=(np.array(shares) * size * divisor).astype(np.uint64),
+                               ts_size=sizes,
                                gap_number=40,
-                               test_size_constant=100)
-    return epsilon, result[0], result[1]
+                               test_size_constant=50)
+    return epsilon,sizes[1], result[0], result[1]
 
-
-def process_share(second_share, size, divisor, epsilon):
-    current_shares = [1 - second_share, second_share]
-    result = threaded_research(r_values=[28, 28 + epsilon],
-                               ts_size=(np.array(current_shares) * size * divisor).astype(np.uint64),
-                               gap_number=40,
-                               test_size_constant=100)
-    return second_share, result[0], result[1]
-
-
-def process_nested(epsilon, size, shares, divisor):
-    result = threaded_research(r_values=[28, 28 + epsilon],
-                               ts_size=(np.array(shares) * size * divisor).astype(np.uint64),
-                               gap_number=40,
-                               test_size_constant=100)
-    return epsilon, size, result[0], result[1]
 
 
 def main():
     divisor = 10
-    base_shares = [0.5, 0.5]
-    size = 40000
-    # Параллелизация цикла по epsilon с использованием процессов
-    epsilons_range = np.arange(1, 30.0, 0.2)
-    rmses = []
-    np_points = []
-    epsilons = []
+
+    #
+    # Параллелизация цикла по размеру 2 ряда
+    second_size_range = [10000,10**5,10**6,10**7]
+    epsilon_range = [1]
+    rmses_sizes = []
+    np_points_sizes = []
+    sizes_list = []
+    epsilon_list = []
 
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        futures = [executor.submit(process_epsilon, epsilon, base_shares, size, divisor)
-                   for epsilon in epsilons_range]
+        futures = [executor.submit(process_size, epsilon,[10000,second_size], divisor)
+                   for second_size in second_size_range for epsilon in epsilon_range]
         for future in concurrent.futures.as_completed(futures):
-            eps, rmse, np_point = future.result()
-            epsilons.append(eps)
-            rmses.append(rmse)
-            np_points.append(np_point)
-
-    # Сортируем результаты по значению epsilon для корректного построения графика
-    sorted_results = sorted(zip(epsilons, rmses, np_points), key=lambda x: x[0])
-    epsilons, rmses, np_points = map(list, zip(*sorted_results))
-
-    plt.figure()
-    sns.regplot(x=epsilons,y= rmses,order = 3)
-    plt.xlabel('Возмущение epsilon')
-    plt.ylabel('Ошибка')
-    plt.title('Зависимость ошибки от epsilon')
-    plt.savefig('./graphics/epsilons_and_rmses.png', dpi=300)
-    plt.show()
-
-    plt.figure()
-    sns.regplot(x=epsilons,y= np_points,order = 3)
-    plt.xlabel('Возмущение epsilon')
-    plt.ylabel('Количество NP точек')
-    plt.title('Зависимость количества непредсказываемых точек от epsilon')
-    plt.savefig('./graphics/epsilons_and_np_points.png')
-    plt.show()
-
-    # Параллелизация цикла по доле второго ряда (shares)
-    second_shares_range = np.arange(0.1, 0.8, 0.01)
-    rmses_shares = []
-    np_points_shares = []
-    shares_list = []
-    epsilon_fixed = 1  # фиксированное значение epsilon
-
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        futures = [executor.submit(process_share, second_share, size, divisor, epsilon_fixed)
-                   for second_share in second_shares_range]
-        for future in concurrent.futures.as_completed(futures):
-            second_share, rmse, np_point = future.result()
-            shares_list.append(second_share)
-            rmses_shares.append(rmse)
-            np_points_shares.append(np_point)
+            epsilon,second_sizes, rmse, np_point = future.result()
+            epsilon_list.append(epsilon)
+            sizes_list.append(second_sizes)
+            rmses_sizes.append(rmse)
+            np_points_sizes.append(np_point)
 
     # Сортировка по доле второго ряда
-    sorted_shares = sorted(zip(shares_list, rmses_shares, np_points_shares), key=lambda x: x[0])
-    shares_list, rmses_shares, np_points_shares = map(list, zip(*sorted_shares))
-
+    sorted_sizes = sorted(zip(sizes_list, rmses_sizes, np_points_sizes), key=lambda x: x[0])
+    sizes_list, rmses_sizes, np_points_sizes = map(list, zip(*sorted_sizes))
     plt.figure()
-    sns.regplot(x=shares_list,y= rmses_shares,order = 3)
-    plt.xlabel('Доля второго ряда')
-    plt.ylabel('Ошибка')
-    plt.title('Зависимость ошибки от доли второго ряда')
-    plt.savefig('./graphics/shares_and_rmses.png', dpi=300)
-    plt.show()
-
-    plt.figure()
-    sns.regplot(x=shares_list,y= np_points_shares,order = 3)
-    plt.xlabel('Доля второго ряда')
+    plt.plot(x=sizes_list, y=np_points_sizes)
+    plt.xscale('log')
+    plt.xlabel('Длина добавленного ряда')
     plt.ylabel('Количество NP точек')
-    plt.title('Зависимость количества непредсказываемых точек от доли второго ряда')
-    plt.savefig('./graphics/shares_and_np_points.png', dpi=300)
+    plt.title('Зависимость количества непредсказываемых точек от size')
+    plt.savefig('./graphics/sizes_and_np_points.png')
     plt.show()
 
+    plt.figure()
+    plt.plot(x=sizes_list, y=rmses_sizes)
+    plt.xscale('log')
+    plt.xlabel('Длина добавленного ряда')
+    plt.ylabel('RMSE')
+    plt.title('Зависимость ошибки от size')
+    plt.savefig('./graphics/sizes_and_rmses.png')
+    plt.show()
 
 if __name__ == '__main__':
     main()
