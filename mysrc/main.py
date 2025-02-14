3da5534581bfe0f8e6cd6c9852e962a4e7886bba
Yaroslav Vasiliev <ikvasilev@edu.hse.ru>
2025-02-14 06:05:41 +0300

    Уменьшенный размер

--- mysrc/main.py
+++ mysrc/main.py
@@ -5,14 +5,14 @@ from matplotlib import pyplot as plt
 import concurrent.futures
 import seaborn as sns
 
-def process_size(epsilon,sizes, divisor):
+
+def process_size(epsilon, sizes, divisor):
     # Каждый процесс выполняет вызов threaded_research с заданным epsilon
     result = threaded_research(r_values=[28, 28 + epsilon],
                                ts_size=sizes,
                                gap_number=20,
                                test_size_constant=50)
-    return epsilon,sizes[1], result[0], result[1]
-
+    return epsilon, sizes[1], result[0], result[1]
 
 
 def main():
@@ -20,8 +20,8 @@ def main():
 
     #
     # Параллелизация цикла по размеру 2 ряда
-    second_size_range = [10**4*10,10**5*10,10**6*10,10**7*10]
-    epsilon_range = [1,5,8,10,15,30]
+    second_size_range = [10 ** 4 * 10, 10 ** 4 * 30, 10 ** 5 * 10, 10 ** 5 * 30, 10 ** 6 * 10]
+    epsilon_range = [1, 5, 8, 10, 15, 30]
     rmses_sizes = []
     np_points_sizes = []
     sizes_list = []
@@ -36,8 +36,8 @@ def main():
             sizes_list.append(second_sizes)
             rmses_sizes.append(rmse)
             np_points_sizes.append(np_point)
-            with open("lol.txt","w") as f:
-                f.write(str(second_sizes)+","+str(np_points_sizes)+","+str(rmse)+"\n")
+            with open("output.txt", "w") as f:
+                f.write(str(second_sizes) + "," + str(np_points_sizes) + "," + str(rmse) + "\n")
     # Сортировка по доле второго ряда
     sorted_sizes = sorted(zip(sizes_list, rmses_sizes, np_points_sizes), key=lambda x: x[0])
     sizes_list, rmses_sizes, np_points_sizes = map(list, zip(*sorted_sizes))
@@ -58,6 +58,7 @@ def main():
     plt.title('Зависимость ошибки от size')
     plt.savefig(f'./graphics/sizes_and_rmses{epsilon}.png', dpi=300)
     plt.show()
+
 
 if __name__ == '__main__':
     main()
