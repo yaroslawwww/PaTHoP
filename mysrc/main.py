6bdcbc7e139d4b22984da7bc659bdc63ee82c81c
Yaroslav Vasiliev <ikvasilev@edu.hse.ru>
2025-03-09 15:17:31 +0300

    final test

--- mysrc/main.py
+++ mysrc/main.py
@@ -11,7 +11,7 @@ import concurrent.futures
 def process_epsilon(deviation, shares, size, divisor):
     result = parallel_research(r_values=[28, 28 + deviation],
                                ts_size=(np.array(shares) * size * divisor).astype(np.uint64),
-                               gap_number=10,
+                               gap_number=1000,
                                test_size_constant=100)
     return deviation, result[0], result[1], result[2]
 
@@ -19,12 +19,12 @@ def process_epsilon(deviation, shares, size, divisor):
 def main():
     divisor = 10
     base_shares = [0.5,0.5]
-    size = 25000
+    size = 250000
     deviation = float(sys.argv[1])
 
     eps, rmse, np_point, affiliation_array = process_epsilon(deviation, base_shares, size, divisor)
 
-    with open(f"/home/ikvasilev/fast_epsilon_counter/fast_epsilon.txt", "a") as f:
+    with open(f"/home/ikvasilev/fast_epsilon_counter/fast_epsilon_with_nice_error.txt", "a") as f:
         f.write(str(deviation) + "," + str(np_point) + "," + str(rmse) + "," + str(affiliation_array) + "\n")
 
 
