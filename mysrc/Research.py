b3f1f8039b1f2d3952b22a91c141139062ea68cc
Yaroslav Vasiliev <ikvasilev@edu.hse.ru>
2025-03-11 00:46:41 +0300

    temp

--- mysrc/Research.py
+++ mysrc/Research.py
@@ -17,9 +17,10 @@ def research(gap_number, r_values, ts_size, window_size, dt,
     divisor = int(0.1 / dt)
     main_ts_size = int(ts_size[0] / divisor)
     list_ts = []
-    value = []
     affiliation_result = []
     for i, r in enumerate(r_values):
+        if ts_size[i] == 0:
+            continue
         ts = TimeSeries("Lorentz", size=ts_size[i], r=r, dt=dt, divisor=divisor)
         list_ts.append(ts)
     window_index = main_ts_size - (gap_number + 1) * window_size
@@ -32,14 +33,14 @@ def research(gap_number, r_values, ts_size, window_size, dt,
     fort, values, last_affiliation = tsproc.pull(epsilon)
     real_values = np.array(list_ts[0].values[window_index:window_index + window_size])
     pred_values = np.array(values[-window_size:])
-    if len(real_values) != len(pred_values):
-        return value
+    print("rv:",real_values)
+    print("pv:",pred_values)
     is_np_point = [0 if pred_values[-1] != np.NaN else 1]
     affiliation_result.append(last_affiliation)
     return pred_values[-1], is_np_point, affiliation_result, real_values[-1]
 
 
-def parallel_research(r_values=None, ts_size=None, gap_number=0, test_size_constant=50, dt=0.01, epsilon=0.01,
+def parallel_research(r_values=None, ts_size=None, gap_number=0, test_size_constant=50, dt=0.01, epsilon=0.001,
                       template_length_constant=4,
                       template_spread_constant=4):
     pred_points_values = []
@@ -51,7 +52,6 @@ def parallel_research(r_values=None, ts_size=None, gap_number=0, test_size_const
                                    dt,
                                    epsilon, template_length_constant, template_spread_constant)
                    for gap in range(gap_number)]
-
         for future in futures:
             result = future.result()
             if result is not None and len(result) > 0:
@@ -59,4 +59,4 @@ def parallel_research(r_values=None, ts_size=None, gap_number=0, test_size_const
                 is_np_points.append(result[1])
                 affiliations_list.append(result[2])
                 real_points_values.append(result[3])
-    return rmse(pred_points_values, real_points_values), np.mean(is_np_points), np.nanmean(affiliations_list)
+    return  pred_points_values, real_points_values, is_np_points, affiliations_list
