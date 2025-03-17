9d12cac3c9c55debf8e05f48fe6a8dfffd9984c6
Yaroslav Vasiliev <ikvasilev@edu.hse.ru>
2025-03-18 01:01:53 +0300

    Расчет для разных доль на 1 ядрн

--- mysrc/zero_case_main.py
+++ mysrc/zero_case_main.py
@@ -20,13 +20,12 @@ def process_epsilon(deviation, shares, size, divisor):
 def main():
     divisor = 10
     shares = [1,0]
-    size = sys.argv[1]
+    size = 25000
     deviation = 0
 
     rmse, np_points , mean_affiliation = process_epsilon(deviation, shares, size, divisor)
-    # print(str(deviation) + "," + str(rmse) + "," + str(np_points) + "," + str(mean_affiliation) + "\n")
     with open(f"/home/ikvasilev/fast_epsilon_counter/rmses_np_points_affiliation_sizes_zero_case.txt", "a") as f:
-        f.write(str(size) + "," + str(rmse) + "," + str(np_points) + "," + str(mean_affiliation) + "\n")
+        f.write(str(shares) + "," + str(rmse) + "," + str(np_points) + "," + str(mean_affiliation) + "\n")
 
 
 if __name__ == '__main__':
