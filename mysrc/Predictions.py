315f3172d951facef2533cfd81586fb663d67f0c
Yaroslav Vasiliev <ikvasilev@edu.hse.ru>
2025-03-29 10:39:58 +0300

    Важное обновление. Повышена читаемость кода, перенесен блок, который замедлял программу, добавлены метки на bash,пересмотрен метод считывания массива.

--- mysrc/Predictions.py
+++ mysrc/Predictions.py
@@ -1,20 +1,97 @@
 # coding: utf-8
-
+from WishartClusterizationAlgorithm import Wishart
+# coding: utf-8
 from TimeSeries import TimeSeries
-from Patterns import Templates
 import numpy as np
 
-from WishartClusterizationAlgorithm import Wishart
+
+class Templates:
+    def __init__(self, template_length, max_template_spread):
+        self.train_set = None
+        self.affiliation_matrix = None
+        self.template_length = template_length
+        self.max_template_spread = max_template_spread
+
+        templates_quantity = max_template_spread ** (template_length - 1)
+        templates = np.zeros((templates_quantity, template_length), dtype=int)
+
+        for i in range(1, template_length):
+            step_size = max_template_spread ** (template_length - i - 1)
+            repeat_count = max_template_spread ** i
+
+            block = np.repeat(np.arange(1, max_template_spread + 1), step_size)
+            templates[:, i] = np.tile(block, repeat_count // max_template_spread) + templates[:, i - 1]
+
+        self.templates = templates
+
+        shapes = np.diff(templates, axis=1)
+        self.observation_indexes = shapes[:, ::-1].cumsum(axis=1)[:, ::-1] * -1
+
+    def add_data_to_train_set(self, data, all_train_sets):
+        if len(data) == 0:
+            return  # Пропускаем пустые ряды
+
+        # Вычисляем размерности для текущего временного ряда
+        x_dim = self.templates.shape[0]
+        y_dim = 0
+        for i in range(x_dim):
+            template_window = self.templates[i][-1]
+            y_dim = max(len(data) - template_window, y_dim)
+
+        z_dim = self.templates.shape[1]
+
+        if y_dim <= 0:
+            return  # Ряд слишком короткий для любых шаблонов
+
+        # Инициализируем train_set для текущего ряда
+        individual_train_set = np.full((x_dim, y_dim, z_dim), np.inf, dtype=float)
+
+        # Заполняем данными
+        for i in range(len(self.templates)):
+            current_template = self.templates[i]
+            template_window = current_template[-1]
+            n_windows = len(data) - template_window
+
+            if n_windows > 0:
+                time_series_indexes = current_template + np.arange(n_windows)[:, None]
+                time_series_vectors = data[time_series_indexes]
+                individual_train_set[i, :n_windows] = time_series_vectors
+
+        all_train_sets.append(individual_train_set)
+
+    def add_data_to_affiliation_matrix(self, data, affiliation_matrix, index):
+        x_dim = self.templates.shape[0]
+        y_dim = 0
+        for i in range(x_dim):
+            template_window = self.templates[i][-1]
+            y_dim = max(len(data) - template_window, y_dim)
+
+        z_dim = self.templates.shape[1]
+        affiliation_matrix.append(np.full((x_dim, y_dim, z_dim), index, dtype=int))
+
+    def create_train_set(self, time_series_list):
+        all_train_sets = []  # Список для хранения train_set каждого временного ряда
+        affiliation_matrix = []
+        for i, time_series in enumerate(time_series_list):
+            if time_series.train is not None:
+                data = np.array(time_series.train)
+            else:
+                data = np.array(time_series.values)
+
+            self.add_data_to_train_set(data, all_train_sets)
+            self.add_data_to_affiliation_matrix(data, affiliation_matrix, i)
+        if all_train_sets:
+            self.train_set = np.concatenate(all_train_sets, axis=1)
+            self.affiliation_matrix = np.concatenate(affiliation_matrix, axis=1)
 
 
-def calc_distance_matrix(test_vectors, train_vectors, steps,
-                         y_dim):
-    # Как будто бы надо попробовать переписать эту функцию под gpu и на понятный язык.
+def calc_distance_matrix(test_vectors, train_vectors):
+    # Как будто бы надо попробовать переписать эту функцию под gpu.
     shape = (test_vectors.shape[0], train_vectors.shape[1])
     distance_matrix = np.zeros(shape)
     for i in range(test_vectors.shape[1]):
-        distance_matrix += (train_vectors[:, :, i] - np.repeat(test_vectors[:, i], y_dim + steps).reshape(-1,
-                                                                                                          y_dim + steps)) ** 2
+        distance_matrix += (train_vectors[:, :, i] - np.repeat(test_vectors[:, i], shape[1]).reshape(-1,
+                                                                                                     shape[1])) ** 2
     distance_matrix **= 0.5
     return distance_matrix
 
@@ -36,17 +113,21 @@ def count_elements_sorted(arr, elements):
 
 
 class TSProcessor:
-    def __init__(self, time_series_list, window_index, template_length,
-                 max_template_spread,
-                 test_size, k=16, mu=0.45):
-        self.time_series_ = time_series_list[0]
-        self.time_series_.split_train_val_test(window_index, test_size)
-        self.templates_ = Templates(template_length, max_template_spread)
-        self.templates_.create_train_set(time_series_list[1:])
-        self.ts_number = len(time_series_list[1:])
+    def __init__(self, k=16, mu=0.45):
+        self.ts_number = None
+        self.templates_ = None
+        self.time_series_ = None
         self.k, self.mu = k, mu
 
-    def pull(self, eps):
+    def fit(self, time_series_list, template_length,
+            max_template_spread):
+        self.templates_ = Templates(template_length, max_template_spread)
+        self.templates_.create_train_set(time_series_list)
+        self.ts_number = len(time_series_list)
+
+    def pull(self, time_series, window_index, test_size, eps):
+        self.time_series_ = time_series
+        self.time_series_.split_train_val_test(window_index, test_size)
         steps = len(self.time_series_.test)
         values = self.time_series_.train
         values += self.time_series_.val
@@ -68,7 +149,7 @@ class TSProcessor:
         affiliation_result = []
         for step in range(steps):
             test_vectors = values[:size_of_series + step][observation_indexes]
-            distance_matrix = calc_distance_matrix(test_vectors, train_vectors, steps, y_dim)
+            distance_matrix = calc_distance_matrix(test_vectors, train_vectors)
             distance_mask = distance_matrix < eps
             points = train_vectors[distance_mask][:, -1]
             affiliation_indexes = affiliation_vectors[distance_mask][:, -1]
@@ -83,10 +164,10 @@ class TSProcessor:
 
     def freeze_point(self, points_pool, how, affiliation_indexes):
         result = None
-        affiliation_result = np.full(self.ts_number,np.NaN)
+        affiliation_result = np.full(self.ts_number, np.NaN)
         if points_pool.size == 0:
             result = np.nan
-            return result,affiliation_result
+            return result, affiliation_result
         if how == 'mean':
             result = float(points_pool.mean())
 
@@ -101,7 +182,8 @@ class TSProcessor:
             wishart.fit(points_pool.reshape(-1, 1))
             # print(np.unique(points_pool,return_counts=True))
             cluster_labels, cluster_sizes = np.unique(wishart.labels_[wishart.labels_ > -1], return_counts=True)
-            if cluster_labels.size > 0 and (np.count_nonzero(((cluster_sizes / cluster_sizes.max()).round(2) > 0.8)) == 1):
+            if cluster_labels.size > 0 and (
+                    np.count_nonzero(((cluster_sizes / cluster_sizes.max()).round(2) > 0.8)) == 1):
                 biggest_cluster_center = points_pool[wishart.labels_ == cluster_labels[cluster_sizes.argmax()]].mean()
 
                 affiliation_result = count_elements_sorted(
--- mysrc/zero_case_main.py
+++ mysrc/zero_case_main.py
@@ -1,6 +1,8 @@
 # coding: utf-8
-import numpy as np
 import sys
+
+import numpy as np
+
 from Research import *
 from matplotlib import pyplot as plt
 import concurrent.futures
@@ -8,22 +10,21 @@ import concurrent.futures
 
 
 def process_deviation(deviation, shares, size):
-    result = parallel_research(r_values=[28,28, 28 + deviation],
-                               ts_size=(np.array([20000] + list(np.array(shares) * size))).astype(np.uint64),
+    # Я брал в качестве тестирующей выборки всегда первый ряд.Остальные создавались для тренировочной.
+    parallel_research(r_values=[28, 28, 28 + deviation],
+                               ts_size=(np.array([np.array(shares)[0] * size + 1250] + list(np.array(shares) * size))).astype(np.uint64),
                                gap_number=1000,
                                test_size_constant=50)
-    return result[0], result[1], result[2]
-
 
 
 
 def main():
     shares = [1,0]
-    size = 25000
-    deviation = 0
-    rmses, np_points , mean_affiliation = process_deviation(deviation, shares, size)
-    with open(f"/home/ikvasilev/fast_epsilon_counter/rmses_np_points_affiliation_.txt", "a") as f:
-        f.write(str(deviation) + "," + str(size) + "," + str(rmses) + "," + str(np_points) + "," + str(mean_affiliation) + "\n")
+    size = 10000
+    deviation = float(sys.argv[1])
+    process_deviation(deviation, shares, size)
+    # with open(f"/home/ikvasilev/fast_epsilon_counter/rmses_np_points_affiliation_.txt", "a") as f:
+    #     f.write(str(deviation) + "," + str(size) + "," + str(rmses) + "," + str(np_points) + "," + str(mean_affiliation) + "\n")
 
 
 if __name__ == '__main__':
