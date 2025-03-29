df8c2c952457349ae5774f3790e08e60db3934cf
Yaroslav Vasiliev <ikvasilev@edu.hse.ru>
2025-03-29 11:32:04 +0300

    234 гига не тянет, к сожалению

--- mysrc/zero_case_main.py
+++ mysrc/zero_case_main.py
@@ -11,20 +11,19 @@ import concurrent.futures
 
 def process_deviation(deviation, shares, size):
     # Я брал в качестве тестирующей выборки всегда первый ряд.Остальные создавались для тренировочной.
-    parallel_research(r_values=[28, 28, 28 + deviation],
+    result = parallel_research(r_values=[28, 28, 28 + deviation],
                                ts_size=(np.array([np.array(shares)[0] * size + 1250] + list(np.array(shares) * size))).astype(np.uint64),
                                gap_number=1000,
                                test_size_constant=50)
-
-
+    return result[0], result[1], result[2],result[3]
 
 def main():
     shares = [1,0]
     size = 10000
     deviation = float(sys.argv[1])
-    process_deviation(deviation, shares, size)
-    # with open(f"/home/ikvasilev/fast_epsilon_counter/rmses_np_points_affiliation_.txt", "a") as f:
-    #     f.write(str(deviation) + "," + str(size) + "," + str(rmses) + "," + str(np_points) + "," + str(mean_affiliation) + "\n")
+    rmses, np_points, mean_affiliation,mape = process_deviation(deviation, shares, size)
+    with open(f"/home/ikvasilev/fast_epsilon_counter/rmses_np_points_affiliation_final_.txt", "a") as f:
+        f.write(str(deviation) + "," + str(size) + "," + str(rmses) + "," + str(np_points) + "," + str(mape) + "," + str(mean_affiliation) + "\n")
 
 
 if __name__ == '__main__':
