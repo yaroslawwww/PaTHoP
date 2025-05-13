a561f557d5511956b2968d28c961bd9b4db996c7
Ярослав <vayark2006@gmail.com>
2025-05-13 15:21:43 +0300

    hist

--- mysrc/Predictions.py
+++ mysrc/Predictions.py
@@ -2,6 +2,8 @@
 import os.path
 import sys
 from collections import defaultdict
+
+from matplotlib import pyplot as plt
 from scipy.spatial.distance import cdist
 from sklearn.cluster import DBSCAN
 from tqdm import tqdm
@@ -66,16 +68,30 @@ class Templates:
 
         all_train_sets.append(individual_train_set)
 
-    def create_train_set(self, time_series):
-        all_train_sets = []  # Список для хранения train_set каждого временного ряда
-        if time_series.train is not None:
-            data = np.array(time_series.train)
-        else:
-            data = np.array(time_series.values)
+    def add_data_to_affiliation_matrix(self, data, affiliation_matrix, index):
+        x_dim = self.templates.shape[0]
+        y_dim = 0
+        for i in range(x_dim):
+            template_window = self.templates[i][-1]
+            y_dim = max(len(data) - template_window, y_dim)
 
-        self.add_data_to_train_set(data, all_train_sets)
+        z_dim = self.templates.shape[1]
+        affiliation_matrix.append(np.full((x_dim, y_dim, z_dim), index, dtype=int))
+
+    def create_train_set(self, time_series_list):
+        all_train_sets = []  # Список для хранения train_set каждого временного ряда
+        affiliation_matrix = []
+        for i, time_series in enumerate(time_series_list):
+            if time_series.train is not None:
+                data = np.array(time_series.train)
+            else:
+                data = np.array(time_series.values)
+
+            self.add_data_to_train_set(data, all_train_sets)
+            self.add_data_to_affiliation_matrix(data, affiliation_matrix, i)
         if all_train_sets:
             self.train_set = np.concatenate(all_train_sets, axis=1)
+            self.affiliation_matrix = np.concatenate(affiliation_matrix, axis=1)
 
 
 def calc_distance_matrix(test_vectors, train_vectors):
@@ -105,7 +121,6 @@ class TSProcessor:
         self.time_series_ = None
         self.k, self.mu = k, mu
         self.motifs = None
-        self.affiliation = None
 
     def fit(self, time_series_list, template_length,
             max_template_spread):
@@ -113,60 +128,105 @@ class TSProcessor:
         self.templates_ = Templates(template_length, max_template_spread)
         wishart = Wishart(k=self.k, mu=self.mu)
         self.motifs = dict()
-        self.affiliation = dict()
-        file_path = f"labels/{max_template_spread}_{sys.argv[1]}_{sys.argv[2]}_{sys.argv[3]}_{sys.argv[4]}_{sys.argv[5]}.npz"
+        self.templates_.create_train_set(time_series_list)
+        file_path = f"../labels/{max_template_spread}_{sys.argv[1]}_{sys.argv[2]}_{sys.argv[3]}_{sys.argv[4]}_{sys.argv[5]}.npz"
         if os.path.exists(file_path):
             save_labels = np.load(file_path)
-            for i, ts in enumerate(time_series_list):
-                self.templates_.create_train_set(ts)
-                z_vectors = self.templates_.train_set
-                for template in tqdm(range(z_vectors.shape[0])):
-                    inf_mask = ~np.isinf(z_vectors[template]).any(axis=1)
-                    temp_z_v = z_vectors[template][inf_mask]
-                    wishart.labels_ = save_labels[f"arr_{template}"]
-                    cluster_labels, cluster_sizes = np.unique(wishart.labels_[wishart.labels_ > -1], return_counts=True)
-                    motifs = [temp_z_v[wishart.labels_ == i].mean(axis=0) for i in cluster_labels]
-                    if template in self.motifs:
-                        self.motifs[template] += list(np.array(motifs).reshape(-1, len(motifs[0])))
-                    else:
-                        self.motifs[template] = list(np.array(motifs).reshape(-1, len(motifs[0])))
-                    if template in self.affiliation:
-                        self.affiliation[template] += list(
-                            np.full(fill_value=i, shape=len(np.array(motifs).reshape(-1, len(motifs[0])))))
-                    else:
-                        self.affiliation[template] = list(
-                            np.full(fill_value=i, shape=len(np.array(motifs).reshape(-1, len(motifs[0])))))
+            z_vectors = self.templates_.train_set
+            for template in tqdm(range(z_vectors.shape[0])):
+                inf_mask = ~np.isinf(z_vectors[template]).any(axis=1)
+                temp_z_v = z_vectors[template][inf_mask]
+                wishart.labels_ = save_labels[f"arr_{template}"]
+                cluster_labels, cluster_sizes = np.unique(wishart.labels_[wishart.labels_ > -1], return_counts=True)
+                motifs = [temp_z_v[wishart.labels_ == i].mean(axis=0) for i in cluster_labels]
+                if template in self.motifs:
+                    self.motifs[template] += list(np.array(motifs).reshape(-1, len(motifs[0])))
+                else:
+                    self.motifs[template] = list(np.array(motifs).reshape(-1, len(motifs[0])))
         else:
             save_labels = []
-            for i, ts in enumerate(time_series_list):
-                self.templates_.create_train_set(ts)
-                z_vectors = self.templates_.train_set
-                for template in tqdm(range(z_vectors.shape[0])):
-                    inf_mask = ~np.isinf(z_vectors[template]).any(axis=1)
-                    temp_z_v = z_vectors[template][inf_mask]
-                    wishart.fit(temp_z_v)
-                    cluster_labels, cluster_sizes = np.unique(wishart.labels_[wishart.labels_ > -1], return_counts=True)
-                    save_labels.append(wishart.labels_)
-                    motifs = [temp_z_v[wishart.labels_ == i].mean(axis=0) for i in cluster_labels]
-                    if template in self.motifs:
-                        self.motifs[template] += list(np.array(motifs).reshape(-1, len(motifs[0])))
-                    else:
-                        self.motifs[template] = list(np.array(motifs).reshape(-1, len(motifs[0])))
-                    if template in self.affiliation:
-                        self.affiliation[template] += list(
-                            np.full(fill_value=i, shape=len(np.array(motifs).reshape(-1, len(motifs[0])))))
-                    else:
-                        self.affiliation[template] = list(
-                            np.full(fill_value=i, shape=len(np.array(motifs).reshape(-1, len(motifs[0])))))
+            z_vectors = self.templates_.train_set
+            for template in tqdm(range(z_vectors.shape[0])):
+                inf_mask = ~np.isinf(z_vectors[template]).any(axis=1)
+                temp_z_v = z_vectors[template][inf_mask]
+                wishart.fit(temp_z_v)
+                cluster_labels, cluster_sizes = np.unique(wishart.labels_[wishart.labels_ > -1], return_counts=True)
+                save_labels.append(wishart.labels_)
+                motifs = [temp_z_v[wishart.labels_ == i].mean(axis=0) for i in cluster_labels]
+                if template in self.motifs:
+                    self.motifs[template] += list(np.array(motifs).reshape(-1, len(motifs[0])))
+                else:
+                    self.motifs[template] = list(np.array(motifs).reshape(-1, len(motifs[0])))
 
             np.savez(file_path, *save_labels)
         for template in self.motifs.keys():
             self.motifs[template] = np.array(self.motifs[template])
-        for template in self.affiliation.keys():
-            self.affiliation[template] = np.array(self.affiliation[template])
+        shares = []
+        cluster_sizes_list = []  # Новый список для хранения размеров кластеров
 
+        save_labels = np.load(file_path)
+        z_vectors = self.templates_.train_set
+        affil_z_vectors = self.templates_.affiliation_matrix
+
+        for template in tqdm(range(z_vectors.shape[0])):
+            inf_mask = ~np.isinf(z_vectors[template]).any(axis=1)
+            temp_z_v = z_vectors[template][inf_mask]
+            temp_z_affil = affil_z_vectors[template][inf_mask]
+            wishart.labels_ = save_labels[f"arr_{template}"]
+
+            cluster_labels, cluster_sizes = np.unique(wishart.labels_[wishart.labels_ > -1], return_counts=True)
+
+            for i, cluster in enumerate(cluster_labels):
+                cluster_mask = (wishart.labels_ == cluster)
+                cluster_size = cluster_sizes[i]
+                affil_in_cluster = (temp_z_affil[cluster_mask, 0] == 1).sum()
+                share = float(affil_in_cluster / cluster_size)
+
+                shares.append(share)
+                cluster_sizes_list.append(cluster_size)  # Сохраняем размер кластера
+
+        # Определяем бины для гистограммы
+        bins = np.linspace(0, 1, 200)  # 10 интервалов от 0 до 1
+        bin_indices = np.digitize(shares, bins) - 1
+        a = 0
+        for i,sh in enumerate(shares):
+            a += (1 - sh) * cluster_sizes_list[i]
+        print(a)
+        # Рассчитываем средние размеры для каждого бина
+        bin_size_sums = np.zeros(len(bins) - 1)
+        bin_counts = np.zeros(len(bins) - 1)
+
+        for idx, size in zip(bin_indices, cluster_sizes_list):
+            if 0 <= idx < len(bins) - 1:
+                bin_size_sums[idx] += size
+                bin_counts[idx] += 1
+
+        mean_sizes = np.divide(bin_size_sums, bin_counts, where=bin_counts != 0)
+
+        # Строим график
+        fig, ax1 = plt.subplots(figsize=(12, 6))
+
+        # Гистограмма долей
+        color = 'tab:blue'
+        ax1.set_xlabel('Доля векторов второго ряда в кластерах', fontsize=12)
+        ax1.set_ylabel('Количество кластеров', color=color, fontsize=12)
+        ax1.hist(shares, bins=bins, edgecolor='black', alpha=0.7, color=color)
+        ax1.tick_params(axis='y', labelcolor=color)
+
+        # Линия средних размеров кластеров
+        ax2 = ax1.twinx()
+        color = 'tab:red'
+        ax2.set_ylabel('Средний размер кластера', color=color, fontsize=12)
+        ax2.plot(bins[:-1] + np.diff(bins) / 2, mean_sizes,
+                 color=color, marker='o', linestyle='--', linewidth=2)
+        ax2.tick_params(axis='y', labelcolor=color)
+
+        plt.title('Распределение долей векторов и средние размеры кластеров', fontsize=14)
+        plt.grid(True, alpha=0.3)
+        plt.tight_layout()
+        plt.show()
+        exit(0)
     def predict(self, time_series, window_index, test_size, eps):
-        print("Predicting\n")
         self.time_series_ = time_series
         self.time_series_.split_train_val_test(window_index, test_size)
         steps = len(self.time_series_.test)
@@ -188,17 +248,12 @@ class TSProcessor:
                 distance_mask = distance_matrix < eps
                 best_motifs = self.motifs[template][distance_mask]
                 motifs_pool.extend(best_motifs)
-                # print(distance_mask.shape)
-                # print(np.array(self.affiliation[template]).shape)
-                # print(self.affiliation[template])
-                motifs_affil.extend(self.affiliation[template][distance_mask])
             motifs_pool = np.array(motifs_pool)
             forecast_point = self.freeze_point(motifs_pool)
             forecast_trajectories[step, 0] = forecast_point
             values[size_of_series + step] = forecast_point
             # print(motifs_affil)
-            mean_affil += np.mean(np.array(motifs_affil)) / steps if len(motifs_affil) > 0 else 0
-        return mean_affil, values
+        return values
 
     def freeze_point(self, motifs_pool):
         if motifs_pool.size == 0:
