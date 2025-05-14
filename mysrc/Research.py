e1b534eab8b132fe76763f2421c9a764b9cf9ddd
Ярослав <vayark2006@gmail.com>
2025-05-14 16:52:03 +0300

    -affil

--- mysrc/Research.py
+++ mysrc/Research.py
@@ -54,14 +54,14 @@ def predict_handler(gap_number, window_size,
     window_index = ts_size - (gap_number + 1) - window_size
     if window_index > len(ts.values) or window_index < 0:
         raise ValueError("Window index out of range")
-    mean_affil, values = ts_processor.predict(ts, window_index, window_size, epsilon)
+    values = ts_processor.predict(ts, window_index, window_size, epsilon)
     real_values = np.array(ts.values[window_index:window_index + window_size])
     pred_values = np.array(values[-window_size:])
     is_np_point = 1 if np.isnan(pred_values[-1]) else 0
     # Проверка результата предсказания на глаз
     mask = ~np.isnan(real_values) & ~np.isnan(pred_values)
     print("res:", abs(real_values[mask] - pred_values[mask]).round(2))
-    return pred_values[-1], is_np_point, real_values[-1], mean_affil
+    return pred_values[-1], is_np_point, real_values[-1]
 
 
 def parallel_research(r_values, ts_size, how_many_gaps, test_size_constant, dt=0.01, epsilon=0.01,
@@ -88,7 +88,5 @@ def parallel_research(r_values, ts_size, how_many_gaps, test_size_constant, dt=0
                 pred_points_values.append(result[0])
                 is_np_points.append(result[1])
                 real_points_values.append(result[2])
-                mean_affils.append(result[3])
     return rmse(pred_points_values, real_points_values), np.mean(is_np_points), mape(pred_points_values,
-                                                                                     real_points_values), np.mean(
-        mean_affils)
+                                                                                     real_points_values)
