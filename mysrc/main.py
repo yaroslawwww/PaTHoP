7048ff4173ac8fc9f8f9b1c5f892f174001a7fa8
Ярослав <vayark2006@gmail.com>
2025-06-05 22:13:38 +0300

    второй ряд выборка

--- mysrc/main.py
+++ mysrc/main.py
@@ -7,18 +7,17 @@ from Research import *
 def main():
     deviation = float(sys.argv[1])
     prediction_size = int(sys.argv[2])
-    general_size = int(sys.argv[3])
-    shares = [float(sys.argv[4]), float(sys.argv[5])]
-    experiment = sys.argv[6]
+    sizes = [5000,int(float(sys.argv[3]))]
+    general_size = 5000 + int(float(sys.argv[3]))
+    experiment = sys.argv[4]
     rmses, np_points, mape = parallel_research(r_values=[28, 28, 28 + deviation],
                                                            ts_size=(np.array(
-                                                               [np.array(shares)[0] * general_size + 1250] + list(
-                                                                   np.array(shares) * general_size))).astype(
+                                                               [5000 + 3250] + sizes).astype(
                                                                np.uint64),
-                                                           how_many_gaps=1000,
+                                                           how_many_gaps=3000,
                                                            test_size_constant=prediction_size)
     with open(f"/home/ikvasilev/PaTHoP/results/{experiment}", "a") as f:
-        f.write(str(deviation) + "," + str(general_size) + "," + str(prediction_size) + "," + str(rmses) + "," + str(
+        f.write(str(deviation) + "," + str(int(sys.argv[3])) + "," + str(prediction_size) + "," + str(rmses) + "," + str(
             np_points) + "," + str(mape) + "," + str(shares[0]) + "\n")
 
 
