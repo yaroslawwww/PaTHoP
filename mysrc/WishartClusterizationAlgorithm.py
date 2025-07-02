38253e5fd9425028a006dafd67712c4f0def4856
Ярослав <vayark2006@gmail.com>
2025-07-02 03:45:46 +0300

    упрощенная система

--- mysrc/WishartClusterizationAlgorithm.py
+++ mysrc/WishartClusterizationAlgorithm.py
@@ -10,13 +10,39 @@ from scipy.spatial.distance import squareform, pdist
 from sklearn.neighbors import NearestNeighbors, BallTree
 from tqdm import tqdm
 
-from QuickSelect import QuickSelect
 # import cupy as cp
 # from cupyx.scipy.spatial.distance import cdist
 # from itertools import product
 from collections import defaultdict
 import math
 
+# coding: utf-8
+def partition(arr, l, r):
+    x = arr[r]
+    i = l
+    for j in range(l, r):
+
+        if arr[j] <= x:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+
+    arr[i], arr[r] = arr[r], arr[i]
+    return i
+
+
+def QuickSelectWithLR(array, left, right, k):
+    if 0 < k <= right - left + 1:
+        index = partition(array, left, right)
+        if index - left == k - 1:
+            return array[index]
+        if index - left > k - 1:
+            return QuickSelectWithLR(array, left, index - 1, k)
+        return QuickSelectWithLR(array, index + 1, right,
+                                 k - index + left - 1)
+
+
+def QuickSelect(array, k):
+    return QuickSelectWithLR(array, 0, len(array) - 1, k)
 
 def volume(radius, dim):
     return np.pi ** (dim / 2) * radius ** dim / gamma(dim / 2 + 1)
